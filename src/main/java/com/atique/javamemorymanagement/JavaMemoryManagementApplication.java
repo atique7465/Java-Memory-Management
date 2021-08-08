606d4b14f8e6bfc4e690c699a9916674fc90761a
atique <atiqurrahman7465@gmail.com>
2021-08-08 16:09:39 +0600

    [Java Memory Management][Load test] initial commit.

---
+++ src/main/java/com/atique/javamemorymanagement/JavaMemoryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.atique.javamemorymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMemoryManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMemoryManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atique/javamemorymanagement/JavaMemoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atique.javamemorymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMemoryManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
